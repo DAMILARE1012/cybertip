6fa8fe16b4c99bf7211048e2cb1cd6422d74702b
DAMILARE1012 <dammy4did@gmail.com>
2022-03-15 12:04:01 +0100

    New Changes

--- database/migrations/2022_03_09_000000_create_users_table.php
+++ database/migrations/2022_03_09_000000_create_users_table.php
@@ -17,9 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('role_id')->unsigned();
-            $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('cascade');
-            // $table->foreignId('role_id')->default(3)->constrained();
+            // $table->integer('role_id')->unsigned();
+            // $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('cascade');
+            $table->foreignId('role_id')->default(3)->constrained();
             // $table->unsignedInteger('role_id')->default(3);
             $table->boolean('admin_approval')->default(0);
             $table->timestamp('email_verified_at')->nullable();
