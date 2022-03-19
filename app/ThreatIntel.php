6abc93e8bb8b150e0af50f146f2f38e1a4131ee3
DAMILARE1012 <dammy4did@gmail.com>
2022-03-19 10:13:46 +0100

    Fakers for ThreatIntel

--- app/ThreatIntel.php
+++ app/ThreatIntel.php
@@ -2,11 +2,15 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class ThreatIntel extends Model
 {
     //
+    use HasFactory;
+
     protected $table = "threat_intels";
     protected $fillable = [
         'alias', 'real_name', 'post', 'url', 'time', 'geolocation', 'source'
