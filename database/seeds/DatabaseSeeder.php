99a1300b873a3fc1fb58a3bedd67e18fe657ad88
DAMILARE1012 <dammy4did@gmail.com>
2022-03-19 10:22:03 +0100

    Fakers for ThreatIntel

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Category', 20)->create();
+        factory('App\ThreatIntel', 20)->create();
         // $this->call(RolesTableSeeder::class);
         // $this->call(UserTableSeeder::class);
         
