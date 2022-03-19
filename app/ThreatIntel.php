02ca072c75e64e03d9a793713a0665c27aa57fae
DAMILARE1012 <dammy4did@gmail.com>
2022-03-19 10:32:47 +0100

    Latest Changes

--- app/ThreatIntel.php
+++ app/ThreatIntel.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ThreatIntel extends Model
 {
     //
-    
+
     protected $table = "threat_intels";
     protected $fillable = [
         'alias', 'real_name', 'post', 'url', 'time', 'geolocation', 'source'
     ];
+
+    public $timestamps = false;
 }
  
