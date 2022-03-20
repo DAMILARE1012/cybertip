17cf4b67b73e240080e99b9dbd512ee848d95503
DAMILARE1012 <dammy4did@gmail.com>
2022-03-20 09:27:47 +0100

    No value

--- database/factories/ThreatIntelFactory.php
+++ database/factories/ThreatIntelFactory.php
@@ -9,7 +9,7 @@ $factory->define(ThreatIntel::class, function (Faker $faker) {
     return [
         'alias' => $faker->userName,
         'real_name' => $faker->name,
-        'post' => $faker->text,
+        'post' => $faker->text(100),
         'url' => $faker->imageUrl,
         'time' => $faker->dateTimeThisYear,
         'geolocation' => $faker->state,
