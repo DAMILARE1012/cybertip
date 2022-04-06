3db8bf5b2a39e7f94624e02b9f825ac6710bb51b
DAMILARE1012 <dammy4did@gmail.com>
2022-04-06 02:23:48 +0100

    one

--- app/ThreatIntel.php
+++ app/ThreatIntel.php
@@ -2,12 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ThreatIntel extends Model
 {
-    use HasFactory;
 
     protected $table = "threat_intels";
     protected $fillable = [
