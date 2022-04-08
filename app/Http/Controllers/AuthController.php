ccaceee29bc17aac49436115f9dc358c7c74cc1b
DAMILARE1012 <dammy4did@gmail.com>
2022-04-08 22:47:35 +0100

    Another One

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -109,7 +109,7 @@ class AuthController extends Controller
         if ($user->admin_approval == 0 && $user->role_id == Role::USER) {
             return response()->json(['message' => 'Dear user, Kindly await the admin approval of your account registration'], 200);
         } else {
-            return response()->json(['message' => ' login successful', 'token' => JWTAuth::fromUser($user), 'User' => $user->with('role')->pluck('role_name'),]);
+            return response()->json(['message' => ' login successful', 'token' => JWTAuth::fromUser($user), 'User' => $user->with('role')->pluck('role'),]);
         }
     }
 
