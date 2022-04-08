01be1b533a55e44f3fd8da5494224424bb70c1ff
DAMILARE1012 <dammy4did@gmail.com>
2022-04-08 23:48:16 +0100

    New Attempadfadfh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -106,11 +106,11 @@ class AuthController extends Controller
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'User credentials not found!'], 400);
         }
-        if ($user->admin_approval == 0 && $user->role->role_id == Role::USER) {
+        if ($user->admin_approval == 0 && $user->role_id == Role::USER) {
             return response()->json(['message' => 'Dear user, Kindly await the admin approval of your account registration'], 200);
-        }elseif ($user->admin_approval == 1 && $user->role->role_id == ROLE::SUPER_ADMIN) {
+        }elseif ($user->admin_approval === 1 && $user->role_id === ROLE::SUPER_ADMIN) {
             return response()->json(['message' => ' login successful', 'token' => JWTAuth::fromUser($user), 'User' => $user, 'role_name' => Role::where('role_name','Super_Admin')->first()->role_name], 200);
-        }elseif($user->admin_approval == 1 && $user->role->role_id == ROLE::USER) {
+        }elseif($user->admin_approval === 1 && $user->role_id === ROLE::USER) {
             return response()->json(['message' => ' login successful', 'token' => JWTAuth::fromUser($user), 'User' => $user, 'role_name' => Role::where('role_name','User')->first()->role_name], 200);
         }
     }
