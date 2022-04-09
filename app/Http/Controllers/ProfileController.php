8f637c2a5144a7caa2f26b12bc376ecde90c15b6
DAMILARE1012 <dammy4did@gmail.com>
2022-04-09 13:29:03 +0100

    Testing

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'companyName' => 'required',
             'domain' => 'required',
@@ -30,8 +31,37 @@ class ProfileController extends Controller
         return response()->json(['Profile' => $profile, 'message' => 'You have successfully registered your company\'s profile. Thanks'], 200);
     }
 
-    public function index(){
-        $digital_profile = Profile::orderBy('created_at', 'desc')->paginate(6);
+    public function index()
+    {
+        $digital_profile = Profile::orderBy('created_at', 'desc')->paginate(2);
         return response()->json($digital_profile, 200);
+    }
+
+    // Edit digital profile....
+    public function edit($id)
+    {
+        $profile = Profile::findOrFail($id);
+        return response()->json($profile, 200);
+    }
+
+    // Update digital profile....
+    public function update(Request $request, $id)
+    {
+        $profile = Profile::find($id);
+
+        $validate = $request->validate([
+            'companyName' => 'required',
+            'domain' => 'required',
+            'ipAddress' => 'required',
+            'keywords' => 'required',
+        ]);
+
+        $profile->companyName = $request->companyName;
+        $profile->domain = $request->domain;
+        $profile->ipAddress = $request->ipAddress;
+        $profile->keywords = $request->keywords;
+
+        $profile->save();
+        return response()->json(['Profile' => $profile, 'message' => 'Profile updated successfully...'], 200);
     }
 }
