25b295f942cae1aaf80759990f3845e727abfa6d
DAMILARE1012 <dammy4did@gmail.com>
2022-04-10 22:35:24 +0100

    Now

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -97,8 +97,7 @@ class UsersController extends Controller
 
     public function searchList($value)
     {
-        $result = User::where('role_id', 2)->orWhere('role_id', 3)->get();
-        $result = $result->where('name', 'LIKE', '%' . $value . '%')->first();
+        $result = User::where('role_id', 2 || 3)->where('name', 'LIKE', '%' . $value . '%')->get();
         if ($result) {
             return Response()->json($result);
         } else {
