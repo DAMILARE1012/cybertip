39b1238dde37645117ae4558419f746203358f0d
DAMILARE1012 <dammy4did@gmail.com>
2022-04-10 23:25:02 +0100

    dafdfadfad

--- app/ActivityRecord.php
+++ app/ActivityRecord.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityRecord extends Model
 {
+    protected $fillable = [
+        'user_id', 'activity_status'
+    ];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
