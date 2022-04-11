934da243a64871a5f0a7842b5ea6b412fbddb272
DAMILARE1012 <dammy4did@gmail.com>
2022-04-11 19:04:24 +0100

    Get Demo Definition

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -118,20 +118,18 @@ class UsersController extends Controller
         return response()->json(['message' => 'Reset passsword page should be here... Kindly create three fields (email, password and password confirmation)'], 201);
     }
 
-    // Invited Members with role - Admin or Analyst Password Reset 
+    // Get demo password....
 
     public function invitationSetPassword(Request $request)
     {
         // Validate input
         $validator = Validator::make($request->all(), [
             'email' => 'required|email|exists:users,email',
-            'name' => 'required',
             'password' => 'required|string|min:6|confirmed',
         ]);
         // Confirm email in the database....
         $user = DB::table('users')->where('email', '=', $request->email)->first();
         $user = User::find($user->id);
-        $user->name = $request->name;
         $user->password = Hash::make($request->password);
         $user->update();
         return response()->json(['message' => 'Password registration successfully done...']);
@@ -144,18 +142,20 @@ class UsersController extends Controller
         return response()->json(['message' => 'Reset passsword page should be here... Kindly create three fields (email, password and password confirmation)'], 201);
     }
 
-    // Get Demo Password (without name)
+    // Invited Members with role - Admin or Analyst Password Reset 
 
     public function resetPassword(Request $request)
     {
         // Validate input
         $validator = Validator::make($request->all(), [
             'email' => 'required|email|exists:users,email',
+            'name' => 'required',
             'password' => 'required|string|min:6|confirmed',
         ]);
         // Confirm email in the database....
         $user = DB::table('users')->where('email', '=', $request->email)->first();
         $user = User::find($user->id);
+        $user->name = $request->name;
         $user->password = Hash::make($request->password);
         $user->update();
         return response()->json(['message' => 'Password registration successfully done...']);
